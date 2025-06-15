d2006a1cdf855c499d26ced15a15f199e22df420
rafaelbau34 <rafaelbaugar@gmail.com>
2025-06-15 01:39:38 -0600

    estructura

---
+++ main.js
@@ -0,0 +1,29 @@
+const cells = document.querySelectorAll(".cells");
+const gameStatus = document.querySelector(".gameStatus");
+const restartBtn = document.querySelector(".restart");
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const currentPlayer = "X";
+const hasStarted = false;
+const cellContent = ["", "", "", "", "", "", "", "", ""];
+
+function startGame() {}
+
+function changePlayer() {}
+
+function cellClick() {}
+
+function cellUpdate() {}
+
+function whoWon() {}
+
+function restartGame() {}
